5ed77bd6c51893eb04ff0cf8b916a2cb8f246f8d
Purbayan Chowdhury <pur.cho.99@gmail.com>
2024-06-15 00:50:08 +0530

    Add darkModeToggler button

--- src/molecules/Footer/Footer.jsx
+++ src/molecules/Footer/Footer.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Footer.scss';
-import { FaCoffee, FaHeart, FaSun, FaMoon } from 'react-icons/fa';
-import Button from '../../atoms/Button/Button';
+import { FaCoffee, FaHeart } from 'react-icons/fa';
+// import Button from '../../atoms/Button/Button';
+import DarkModeToggler from "../../atoms/DarkModeToggler/DarkModeToggler";
 
 function Footer({ darkModeToggler, isDark, ...otherProps }) {
 	return (
 		<section className="Footer">
 			<div className="Footer__buttons">
-				<Button
-					className=""
-					onClick={() => {
-						darkModeToggler();
-					}}
-                    title="Dark Mode Toggler"
-				>
-					{isDark ? <FaMoon /> : <FaSun />}
-				</Button>
+                <DarkModeToggler
+                    darkModeToggler={darkModeToggler}
+                    isDark={isDark}
+                />
 			</div>
 			<div className="Copyright">
 				Made with <FaHeart className="red-heart" /> and <FaCoffee className="green-tea" /> by Purbayan Chowdhury
@@ -30,7 +26,7 @@ Footer.propTypes = {
 };
 
 Footer.defaultProps = {
-	darkMode: false,
+	isDark: false,
 };
 
 export default Footer;
