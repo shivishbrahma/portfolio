06bbc793dd5f0957888cb4972a12ada78411a9cb
Purbayan Chowdhury <pur.cho.99@gmail.com>
2024-08-16 20:36:13 +0530

    update Typewriter.jsx

--- src/atoms/Typewriter/Typewriter.jsx
+++ src/atoms/Typewriter/Typewriter.jsx
@@ -1,138 +1,137 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Typewriter.scss';
-import Cursor from './Cursor';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Typewriter.scss";
+import Cursor from "./Cursor";
 
 function Typewriter({
-	text,
-	cursor,
-	cursorClassName,
-	cursorRenderer,
-	displayTextRenderer,
-	speed,
-	eraseSpeed,
-	typingDelay,
-	eraseDelay,
-	...otherProps
+    text,
+    cursor,
+    cursorClassName,
+    cursorRenderer,
+    displayTextRenderer,
+    speed,
+    eraseSpeed,
+    typingDelay,
+    eraseDelay,
+    ...otherProps
 }) {
-	const [currentText, setCurrentText] = React.useState('');
-	const [currentIndex, setCurrentIndex] = React.useState(0);
-	const [isTyping, setIsTyping] = React.useState(true);
-	const [interval, setInterval] = React.useState(null);
+    const [currentText, setCurrentText] = React.useState("");
+    const [currentIndex, setCurrentIndex] = React.useState(0);
+    const [isTyping, setIsTyping] = React.useState(true);
+    const [interval, setInterval] = React.useState(null);
 
-	React.useEffect(() => {
-		startTyping();
+    React.useEffect(() => {
+        startTyping();
 
-		return () => {
-			interval && clearTimeout(interval);
-		};
+        return () => {
+            interval && clearTimeout(interval);
+        };
 
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-	React.useEffect(() => {
-		let rawText = getRawText()[currentIndex];
-		if (isTyping) {
-			if (currentText.length < rawText.length) {
-				setInterval(setTimeout(type, speed));
-			} else {
-				setIsTyping(false);
-				setInterval(setTimeout(erase, eraseDelay));
-			}
-		} else {
-			if (currentText.length === 0) {
-				const textArray = getRawText();
-				let index = currentIndex + 1 === textArray.length ? 0 : currentIndex + 1;
-				if (index === currentIndex) {
-					setIsTyping(true);
-					setTimeout(startTyping, typingDelay);
-				} else {
-					setTimeout(() => setCurrentIndex(index), typingDelay);
-				}
-			} else {
-				setInterval(setTimeout(erase, eraseSpeed));
-			}
-		}
-		return () => {
-			interval && clearTimeout(interval);
-		};
+    React.useEffect(() => {
+        let rawText = getRawText()[currentIndex];
+        if (isTyping) {
+            if (currentText.length < rawText.length) {
+                setInterval(setTimeout(type, speed));
+            } else {
+                setIsTyping(false);
+                setInterval(setTimeout(erase, eraseDelay));
+            }
+        } else if (currentText.length === 0) {
+            const textArray = getRawText();
+            let index = currentIndex + 1 === textArray.length ? 0 : currentIndex + 1;
+            if (index === currentIndex) {
+                setIsTyping(true);
+                setTimeout(startTyping, typingDelay);
+            } else {
+                setTimeout(() => setCurrentIndex(index), typingDelay);
+            }
+        } else {
+            setInterval(setTimeout(erase, eraseSpeed));
+        }
 
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [currentText]);
+        return () => {
+            interval && clearTimeout(interval);
+        };
 
-	React.useEffect(() => {
-		if (!isTyping) {
-			setIsTyping(true);
-			startTyping();
-		}
-		return () => {
-			interval && clearTimeout(interval);
-		};
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentText]);
 
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [currentIndex]);
+    React.useEffect(() => {
+        if (!isTyping) {
+            setIsTyping(true);
+            startTyping();
+        }
+        return () => {
+            interval && clearTimeout(interval);
+        };
 
-	function getRawText() {
-		return typeof text === 'string' ? [text] : [...text];
-	}
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentIndex]);
 
-	function startTyping() {
-		setInterval(
-			setTimeout(() => {
-				type();
-			}, speed)
-		);
-	}
+    function getRawText() {
+        return typeof text === "string" ? [text] : [...text];
+    }
 
-	function type() {
-		let rawText = getRawText()[currentIndex];
+    function startTyping() {
+        setInterval(
+            setTimeout(() => {
+                type();
+            }, speed)
+        );
+    }
 
-		if (currentText.length < rawText.length) {
-			let displayText = rawText.substr(0, currentText.length + 1);
-			setCurrentText(displayText);
-		}
-	}
+    function type() {
+        let rawText = getRawText()[currentIndex];
 
-	function erase() {
-		let index = currentIndex;
-		if (currentText.length === 0) {
-			// console.log('Start Typing');
-			const textArray = getRawText();
-			index = index + 1 === textArray.length ? 0 : index + 1;
-			setCurrentIndex(index);
-		} else {
-			let displayText = currentText.substr(-currentText.length, currentText.length - 1);
-			setCurrentText(displayText);
-		}
-	}
+        if (currentText.length < rawText.length) {
+            let displayText = rawText.substr(0, currentText.length + 1);
+            setCurrentText(displayText);
+        }
+    }
 
-	return (
-		<div className="Typewriter" {...otherProps}>
-			<span className="Typewriter__text">
-				{displayTextRenderer ? displayTextRenderer(currentText, currentIndex) : currentText}
-			</span>
-			<Cursor cursor={cursor} cursorRenderer={cursorRenderer} cursorClassName={cursorClassName} />
-		</div>
-	);
+    function erase() {
+        let index = currentIndex;
+        if (currentText.length === 0) {
+            // console.log('Start Typing');
+            const textArray = getRawText();
+            index = index + 1 === textArray.length ? 0 : index + 1;
+            setCurrentIndex(index);
+        } else {
+            let displayText = currentText.substr(-currentText.length, currentText.length - 1);
+            setCurrentText(displayText);
+        }
+    }
+
+    return (
+        <div className="Typewriter" {...otherProps}>
+            <span className="Typewriter__text">
+                {displayTextRenderer ? displayTextRenderer(currentText, currentIndex) : currentText}
+            </span>
+            <Cursor cursor={cursor} cursorRenderer={cursorRenderer} cursorClassName={cursorClassName} />
+        </div>
+    );
 }
 
 Typewriter.propTypes = {
-	speed: PropTypes.number.isRequired,
-	eraseSpeed: PropTypes.number.isRequired,
-	typingDelay: PropTypes.number.isRequired,
-	eraseDelay: PropTypes.number.isRequired,
-	cursorClassName: PropTypes.string,
-	cursor: PropTypes.string,
-	text: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.string), PropTypes.string]).isRequired,
-	cursorRenderer: PropTypes.func,
-	displayTextRenderer: PropTypes.func,
+    speed: PropTypes.number.isRequired,
+    eraseSpeed: PropTypes.number.isRequired,
+    typingDelay: PropTypes.number.isRequired,
+    eraseDelay: PropTypes.number.isRequired,
+    cursorClassName: PropTypes.string,
+    cursor: PropTypes.string,
+    text: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.string), PropTypes.string]).isRequired,
+    cursorRenderer: PropTypes.func,
+    displayTextRenderer: PropTypes.func
 };
 
 Typewriter.defaultProps = {
-	speed: 200,
-	eraseSpeed: 200,
-	typingDelay: 2500,
-	eraseDelay: 5000,
+    speed: 200,
+    eraseSpeed: 200,
+    typingDelay: 2500,
+    eraseDelay: 5000
 };
 
 export default Typewriter;
