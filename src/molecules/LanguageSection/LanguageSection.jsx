4377bc881c508bd5c340c26a84cef161a9abb13c
Purbayan Chowdhury <pur.cho.99@gmail.com>
2024-08-19 00:10:49 +0530

    update 5 files

--- src/molecules/LanguageSection/LanguageSection.jsx
+++ src/molecules/LanguageSection/LanguageSection.jsx
@@ -4,6 +4,8 @@ import PageSection from "@/atoms/PageSection/PageSection";
 // import PropTypes from "prop-types";
 import { TagCloud } from "@frank-mayer/react-tag-cloud";
 
+import "./LanguageSection.scss";
+
 function LanguageSection({ ...otherProps }) {
     const elems = [
         { text: "Javascript", weight: 1 },
@@ -15,15 +17,19 @@ function LanguageSection({ ...otherProps }) {
         { text: "MySQL", weight: 1 }
     ];
 
+    const [lang, setLang] = React.useState(elems[0].text);
+
     return (
         <PageSection sectionTitle="Programming Languages" {...otherProps}>
             {/* <Globe3D tags={elems} radius={150} /> */}
             <TagCloud
                 options={(w) => ({
                     radius: Math.min(500, w.innerWidth, w.innerHeight) / 2,
-                    maxSpeed: "normal"
+                    maxSpeed: "normal",
+                    itemClass: "TagCloud__item",
+                    containerClass: "TagCloud__container",
                 })}
-                onClick={(tag, evt) => alert(tag)}
+                onClick={(tag, evt) => setLang(tag)}
                 onClickOptions={{ passive: true }}
             >
                 {elems.map((elem) => elem.text)}
